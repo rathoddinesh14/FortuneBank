0e15747ff2812e3460153ac8d1067d59960d327d
Rathod Dinesh <dinesh.rathod.c2019@iitbombay.org>
2023-08-11 13:07:28 +0530

    added frontend

---
+++ fortune-bank-frontend/src/components/UserRegistration.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class UserRegistration extends Component {
+    render() {
+        return(
+            <h1>User Registration page</h1>
+        );
+    }
+}
+
+export default UserRegistration;
