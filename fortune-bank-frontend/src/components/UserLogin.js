76ea5f93c2ceb3dc23e27e528517ce845dea4a35
Rathod Dinesh <85003035+rathoddinesh14@users.noreply.github.com>
2023-08-16 00:02:39 +0530

    added forms (#4)

--- fortune-bank-frontend/src/components/UserLogin.js
+++ fortune-bank-frontend/src/components/UserLogin.js
@@ -1,11 +1,62 @@
-import React, {Component} from 'react';
+import "./../styles/UserLogin.css";
+import React, { useState } from "react";
+import axios from "axios"; // Import axios
 
-class UserLogin extends Component {
-    render() {
-        return(
-            <h1>Login page</h1>
-        );
-    }
+function UserLogin() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      username: username,
+      password: password,
+    };
+
+    // Send POST request using axios
+    axios
+      .post("http://your-spring-boot-api-url/login", data)
+      .then((response) => {
+        console.log("Login successful:", response.data);
+      })
+      .catch((error) => {
+        console.error("Login failed:", error);
+      });
+  };
+
+  return (
+    <div className="container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+
+        <label htmlFor="password">Password: </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default UserLogin;
