5aaae788fc64a78d1fb18f7b0a4b339902509d4a
64bitmachine <85003035+rathoddinesh14@users.noreply.github.com>
2023-08-28 10:42:48 +0530

    added javadocs to the enum classes

--- FortuneBank/src/main/java/com/fortunebank/user/enumtype/AccountStatus.java
+++ FortuneBank/src/main/java/com/fortunebank/user/enumtype/AccountStatus.java
@@ -1,15 +1,28 @@
 package com.fortunebank.user.enumtype;
 
+/**
+ * Enum representing different account status types.
+ */
 public enum AccountStatus {
     ENABLED("ENABLED"),
     DISABLED("DISABLED");
 
     private final String displayName;
 
+    /**
+     * Constructs an AccountStatus enum value with a corresponding display name.
+     *
+     * @param displayName The display name associated with the account status.
+     */
     AccountStatus(String displayName) {
         this.displayName = displayName;
     }
 
+    /**
+     * Returns the display name of the account status.
+     *
+     * @return The display name of the account status.
+     */
     public String getDisplayName() {
         return displayName;
     }
