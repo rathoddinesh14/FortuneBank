fde9048f4b17e604c778b4203c9b03a29efb6635
64bitmachine <85003035+rathoddinesh14@users.noreply.github.com>
2023-09-24 18:36:15 +0530

    added basic auth

--- FortuneBank/src/main/java/com/fortunebank/FortuneBankApplication.java
+++ FortuneBank/src/main/java/com/fortunebank/FortuneBankApplication.java
@@ -2,8 +2,10 @@ package com.fortunebank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class FortuneBankApplication {
 
 	public static void main(String[] args) {
