6650245a8691eafb39d969469cd835ee56b58de0
Rathod Dinesh <dinesh.rathod.c2019@iitbombay.org>
2024-07-18 22:13:50 +0530

    added navigation step

--- fortune-bank-frontend/src/test/steps/navigationSteps.ts
+++ fortune-bank-frontend/src/test/steps/navigationSteps.ts
@@ -1,5 +1,23 @@
 import { Given } from "@cucumber/cucumber";
+import { chromium, Page, Browser, expect } from "@playwright/test";
+
+let browser: Browser;
+let page: Page;
 
 Given('User navigates to the application', async function () {
-  console.log('hello');
+    browser = await chromium.launch({ headless: false });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+
+    // Locate the Login button
+    const loginButton = page.locator('button', { hasText: 'Login' });
+
+    // Click the Login button
+    await loginButton.click();
+
+    // Assert that the URL changed to the login page
+    await expect(page).toHaveURL('http://localhost:3000/login');
+    await page.close()
+    await browser.close()
 });
+
